9c938ffca6cedd9d4f9e7e817a299b9e495a2499
kamalkharel5 <kamalkharel5@gmail.com>
2026-06-18 13:59:30 -0400

    Version 4.0 - broken

--- main.py
+++ main.py
@@ -1,11 +1,20 @@
-import os
-from flask import Flask
+# import os
+# from flask import Flask
 
+# app = Flask(__name__)
+# version = os.getenv("APP_VERSION", "unknown")
+
+# @app.route('/')
+# def index():
+#     return f'Hello Argo CD {version}!'
+
+# app.run(host='0.0.0.0', port=8080)
+
+from flask import Flask
 app = Flask(__name__)
-version = os.getenv("APP_VERSION", "unknown")
 
 @app.route('/')
 def index():
-    return f'Hello Argo CD {version}!'
+    return 'v4.0.0 - BROKEN VERSION!'  # Pretend this is broken
 
 app.run(host='0.0.0.0', port=8080)
